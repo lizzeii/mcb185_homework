91cf0045ba426d570bf6c9f4b3962feb1c0265be
lizzeii <liz.kong.21@gmail.com>
2025-02-04 19:40:08 -0800

    update

--- 04spellingbee.sh
+++ 04spellingbee.sh
@@ -1,3 +1,6 @@
+#softlink
+ln -s ../MCB185/data/dictionary.gz ./dictionary.gz
+
 #original command
 gunzip -c dictionary.gz | grep -E "r" | grep -E "[acinozr].{2}+[acinozr]" | grep -v "[bdefghjklmpqstuvwxy]"
 
